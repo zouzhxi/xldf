416069792ac1fb1cbcbca18c583b02bd5ff2e7d7
86 <zouzhxi@hotmail.com>
2022-04-22 15:38:20 +0800

    add xldf 220422

--- data/mdd_data_2022.js
+++ data/mdd_data_2022.js
@@ -117,6 +117,7 @@ var datalist2={
 { day:18, sale:87810.11, mt:29364.66, el:11055.32,weimob:18940.79, pay:68300 ,shgpay:33700,hydpay:32600,qjdpay:2000,shgsale:109284.5,hydsale:39819.9,qjdsale:3230.9},
 { day:19, sale:96669.176, mt:24402.52, el:10845.44,weimob:33764.24, pay:80000 ,shgpay:30400,hydpay:47500,qjdpay:2100,shgsale:106084.186,hydsale:72152.32,qjdsale:3399.9},
 { day:20, sale:89510.01, mt:25264.28, el:10105.02,weimob:23376.25, pay:67300 ,shgpay:29200,hydpay:36800,qjdpay:1300,shgsale:102879.3,hydsale:50640.8,qjdsale:4830.1},
+{ day:21, sale:100714.49, mt:20596.3, el:9181.4,weimob:28846.21, pay:72900 ,shgpay:26700,hydpay:46200,qjdpay:0,shgsale:100755,hydsale:66965,qjdsale:3212.7},
 
 		]}
 	]
