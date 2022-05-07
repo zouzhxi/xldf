35f918999d112d3968017933daa2f27bf9ed1067
CHIH <zouzhxi@hotmail.com>
2022-05-07 09:24:45 +0800

    Update mdd_data_2022.js

--- data/mdd_data_2022.js
+++ data/mdd_data_2022.js
@@ -134,6 +134,7 @@ var datalist2={list:[
 { day:3, sale:99317.33, mt:18331.92, el:5449.12,weimob:37723.14, pay:93300 ,shgpay:41600,hydpay:50700,qjdpay:1000,shgsale:101089.7,hydsale:68797.1,qjdsale:3223},
 { day:4, sale:116444.71, mt:21934.24, el:6762.94,weimob:38983.92, pay:105379 ,shgpay:44579,hydpay:49100,qjdpay:11700,shgsale:107322.7,hydsale:73397.4,qjdsale:15082.9},
 { day:5, sale:94380.7, mt:25998.82, el:7723.78,weimob:35771, pay:97300 ,shgpay:41300,hydpay:45500,qjdpay:10500,shgsale:105123.3,hydsale:65378,qjdsale:2630.5},
+{ day:6, sale:97443.91, mt:26154.36, el:7691.84,weimob:37859.7, pay:95700 ,shgpay:40000,hydpay:55200,qjdpay:500,shgsale:107180.5,hydsale:75863.54,qjdsale:3792.1},
 
 ]}
 
