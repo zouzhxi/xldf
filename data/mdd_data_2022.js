8c4bf378e09e313a8852b83369b4ef86e1095e68
zouzhxi <zouzhxi@hotmail.com>
2022-05-28 09:52:18 +0800

    add 528

--- data/mdd_data_2022.js
+++ data/mdd_data_2022.js
@@ -155,6 +155,8 @@ var datalist2={list:[
 { day:24, sale:94673.7, mt:23355.4, el:7209.52,weimob:26940.5, pay:58100 ,shgpay:29800,hydpay:27100,qjdpay:1200,shgsale:103566.3,hydsale:50673.9,qjdsale:5235.8},
 { day:25, sale:96243.47, mt:24351.62, el:7300.5,weimob:24438.74, pay:52300 ,shgpay:26700,hydpay:23800,qjdpay:1800,shgsale:101855.4,hydsale:45507,qjdsale:10865.2},
 	{ day: 26, sale: 93065.85, mt: 22252.4, el: 8211.06, weimob: 23069.75, pay: 52000, shgpay: 27400, hydpay: 22400, qjdpay: 2200, shgsale: 99830.5, hydsale: 42841.2, qjdsale: 8702.7 },
+	{ day: 27, sale: 93169.5, mt: 25699.92, el: 10043.9, weimob: 0, pay: 43600, shgpay: 19900, hydpay: 22600, qjdpay: 1100, shgsale: 107877.9, hydsale: 46487.7, qjdsale: 8886.7 },
+
 ]}
 
 ]};
