30971209d2010f71b8a33eefc0ec1434547ec7a3
zouzhxi <zouzhxi@hotmail.com>
2022-06-22 09:00:49 +0800

    add xldf 622

--- data/mdd_data_2022.js
+++ data/mdd_data_2022.js
@@ -181,6 +181,7 @@ var datalist2={list:[
 	{ day: 18, sale: 116674.668, mt: 33751.64, el: 11628.66, weimob: 86769.15, pay: 105100, shgpay: 31500, hydpay: 64500, qjdpay: 9100, shgsale: 137335.62, hydsale: 127082.278, qjdsale: 11607.1 },
 	{ day: 19, sale: 110285.46, mt: 35531.43, el: 11844.22, weimob: 26874.08, pay: 94300, shgpay: 38900, hydpay: 50200, qjdpay: 5200, shgsale: 129032.9, hydsale: 63374.4, qjdsale: 9948.6 },
 	{ day: 20, sale: 95761.03, mt: 30647.65, el: 10403.1, weimob: 25130.96, pay: 87400, shgpay: 33900, hydpay: 40500, qjdpay: 13000, shgsale: 114405.7, hydsale: 53259.9, qjdsale: 6799.3 },
+	{ day: 21, sale: 97301.076, mt: 30710.12, el: 9642, weimob: 25790.63, pay: 78000, shgpay: 29000, hydpay: 46600, qjdpay: 2400, shgsale: 116080.9, hydsale: 54619.006, qjdsale: 1968.8 },
 
 ]}
 
