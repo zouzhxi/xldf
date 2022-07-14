17a03c1c91d51ab856cf377dc50e315ac10035b7
zouzhxi <zouzhxi@hotmail.com>
2022-07-14 09:20:23 +0800

    add xldf 714

--- data/mdd_data_2022.js
+++ data/mdd_data_2022.js
@@ -205,6 +205,7 @@ var datalist2={list:[
 	{ day: 10, sale: 116724.9, mt: 31596, el: 13038.34, weimob: 44268.38, pay: 73300, shgpay: 33900, hydpay: 27500, qjdpay: 11900, shgsale: 120777.6, hydsale: 71120.9, qjdsale: 16850 },
 	{ day: 11, sale: 125081.46, mt: 30719.94, el: 11555.86, weimob: 39786.61, pay: 63900, shgpay: 28400, hydpay: 23600, qjdpay: 11900, shgsale: 117563.9, hydsale: 70966, qjdsale: 30926 },
 	{ day: 12, sale: 105381.118, mt: 34196.89, el: 11920.26, weimob: 46483.96, pay: 63300, shgpay: 28900, hydpay: 33700, qjdpay: 700, shgsale: 124591.9, hydsale: 70992.918, qjdsale: 6259.3 },
+	{ day: 13, sale: 100623.88, mt: 34275.89, el: 16309.36, weimob: 39379.55, pay: 55800, shgpay: 27400, hydpay: 27700, qjdpay: 700, shgsale: 125459.6, hydsale: 66115.3, qjdsale: 6961.4 },
 
 ]}
 
