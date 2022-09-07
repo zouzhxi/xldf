f6930903220e099023ebd25db9dc63b9920eb068
zouzhxi <zouzhxi@hotmail.com>
2022-09-07 10:25:28 +0800

    add 907

--- data/mdd_data_2022.js
+++ data/mdd_data_2022.js
@@ -265,6 +265,7 @@ var datalist2={list:[
 	{ day: 3, sale: 78632.81, mt: 25666.2, el: 14479.97, weimob: 41574.9, pay: 55400, shgpay: 14700, hydpay: 40700, qjdpay: 0, shgsale: 81521.56, hydsale: 93077.15, qjdsale: 1780.4 },
 	{ day: 4, sale: 67006.57, mt: 29215.11, el: 15442.94, weimob: 50740.07, pay: 42100, shgpay: 9400, hydpay: 32700, qjdpay: 0, shgsale: 72390.2, hydsale: 77136.85, qjdsale: 1593.7 },
 	{ day: 5, sale: 105533.31, mt: 31112.24, el: 15267.4, weimob: 32734.43, pay: 56100, shgpay: 25600, hydpay: 29100, qjdpay: 1400, shgsale: 113849.6, hydsale: 72167.4, qjdsale: 4704.9 },
+	{ day: 6, sale: 98310.81, mt: 29755.36, el: 15284.4, weimob: 28924.34, pay: 53900, shgpay: 22600, hydpay: 31300, qjdpay: 0, shgsale: 102371.6, hydsale: 70949.5, qjdsale: 4083.5 },
 
 ]}
 
